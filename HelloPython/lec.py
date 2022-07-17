3e31db54c748f87a76d0a1c1bf0bbaa95f7b4f84
Sere0708 <103060203+Sere0708@users.noreply.github.com>
2022-07-18 00:08:42 +0300

    .\HelloPython\

--- HelloPython/lec.py
+++ HelloPython/lec.py
@@ -64,15 +64,15 @@
 # A (7,-5); B (1,-1) -> 7,21
 
 
-print('Enter coordinates point А:')
-x_A = float(input('X: '))
-y_A = float(input('Y: '))
-print("Enter coordinates point B:")
-x_B = float(input('X: '))
-y_B = float(input('Y: '))
+# print('Enter coordinates point А:')
+# x_A = float(input('X: '))
+# y_A = float(input('Y: '))
+# print("Enter coordinates point B:")
+# x_B = float(input('X: '))
+# y_B = float(input('Y: '))
 
-from math import sqrt
-print('Distance between A and B: ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2))
+# from math import sqrt
+# print('Distance between A and B: ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2))
 
 
 
